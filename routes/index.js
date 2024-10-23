747c737a1209d3dfb0b62141cc5893afca013111
aingeru.garcia <aingeru.garcia@geoaitech.com>
2024-10-23 14:13:28 +0200

    Change express' default title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Skills / Sistemas Web (2024-2025)' });
 });
 
 module.exports = router;
