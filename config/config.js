99d7b72e97b07fa74aac5f3a2cf223870c4f7510
geru-scotland <aingeru.scotland@gmail.com>
2024-10-25 19:17:55 +0200

    [image-harvester] Start implementing image harvester

---
+++ config/config.js
@@ -0,0 +1,8 @@
+
+const configMap = {
+    skill_endpoint: "https://tinkererway.dev/web_skill_trees/electronics_skill_tree",
+    icon_endpoint: "https://tinkererway.dev/web_skill_trees_resources/svg/electronics_icons",
+    icon_path: "public/electronics/icons"
+}
+
+module.exports = configMap;
